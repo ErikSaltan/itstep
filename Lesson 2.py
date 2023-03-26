59f2839ac2eb73f268cdd347a1699da387eb04d4
ErikSaltan <saltanerik2000@gmail.com>
2023-03-26 23:33:00 +0300

    add some changes

--- Lesson 2.py
+++ Lesson 2.py
@@ -5,6 +5,7 @@ class Student:
         self.name = name
         self.gladness = 50
         self.progress = 0
+        self.money = 45
         self.alive = True
 
 
@@ -21,8 +22,25 @@ class Student:
 
     def to_chill(self):
         print("Rest time")
-        self.gladness += 5
+        self.gladness += 9
         self.progress -= 0.1
+        self.money -= 5
+
+    def to_work(self):
+        print("Time to work!")
+        self.gladness -= 5
+        self.money += 8
+
+    def hard_work(self):
+        print("Not enough money... I'll work a little harder!")
+        self.gladness -= 20
+        self.money += 45
+
+    def additional_courses(self):
+        print("I study badly... Worth attending additional courses!")
+        self.progress += 1.3
+        self.money -= 30
+        self.gladness -= 10
 
 
     def is_alive(self):
@@ -35,18 +53,22 @@ class Student:
         elif self.progress > 5:
             print("Passed externally…")
             self.alive = False
+        elif self.money <= 0:
+            print("Bankrupt…")
+            self.alive = False
 
 
     def end_of_day(self):
         print(f"Gladness = {self.gladness}")
         print(f"Progress = {round(self.progress, 2)}")
+        print(f"Money = {self.money}")
 
 
     def live(self, day):
         day = "Day" + str(day) + "of" + self.name + "life"
         print(f"{day:=^50}")
 
-        live_cube = random.randint(1, 3)
+        live_cube = random.randint(1, 4)
 
         if live_cube == 1:
             self.to_study()
@@ -54,6 +76,14 @@ class Student:
             self.to_sleep()
         elif live_cube == 3:
             self.to_chill()
+        elif live_cube == 4:
+            self.to_work()
+
+        if -0.49 <= self.progress <= -0.15 and self.money >= 150:
+            self.additional_courses()
+
+        if self.gladness >= 145 and self.money <= 30:
+            self.hard_work()
 
         self.end_of_day()
         self.is_alive()
@@ -61,6 +91,6 @@ class Student:
 
 nick = Student(name="Nick")
 
-for day in range(365):
+for day in range(366):
     if nick.alive:
         nick.live(day)
